a341b0638211243c9b5e82088b187c863d3bbe91
Glitch (botkit-slack) <none>
2019-02-26 14:53:19 +0000

    🌝🐋 Checkpoint ./skills/sample_conversations.js:3258657/510

--- skills/sample_conversations.js
+++ skills/sample_conversations.js
@@ -25,6 +25,21 @@ module.exports = function(controller) {
         });
 
     });
+  
+  controller.hears(['button'], 'direct_message,direct_mention', function(bot, message) {
+
+        bot.startConversation(message, function(err, convo) {
+            convo.say('This is an example of using convo.ask with a single callback.');
+
+            convo.ask('What is a button?', function(response, convo) {
+
+                convo.say('Cool, I like ' + response.text + ' too!');
+                convo.next();
+
+            });
+        });
+
+    });
 
 
     controller.hears(['question'], 'direct_message,direct_mention', function(bot, message) {
